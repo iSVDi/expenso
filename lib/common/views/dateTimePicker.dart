3c48460119c0ca0e21f9e69e0346aac532a1b458
Daniil-Shestopalov <d.v.shestopalov@mail.ru>
2024-03-25 11:52:13 +0300

    made datePicker ui as in design

--- lib/common/views/dateTimePicker.dart
+++ lib/common/views/dateTimePicker.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import "package:expenso/extensions/appColors.dart";
 import "package:flutter/material.dart";
 
 import "package:expenso/extensions/dateTime.dart";
@@ -19,31 +20,40 @@ class DateTimePicker extends StatefulWidget {
 }
 
 class _DateTimePickerState extends State<DateTimePicker> {
-  final double _screenHeightRatio = 0.2;
+  final double _screenHeightRatio = 0.3;
+  final double _screenWidthRatio = 0.9;
 
   @override
   Widget build(BuildContext context) {
-    var cancelButton = TextButton(
-        onPressed: () {
-          widget.callback(null);
-        },
-        child: Text("Cancel")); //TODO: move text to special class
-    var applyButton = TextButton(
-        onPressed: () {
-          widget.callback(widget._selectedDate);
-        },
-        child: Text("Apply")); //TODO: move text to special class
+    var cancelButton = _getRoundedButton(() {
+      widget.callback(null);
+    }, "cancel", AppColors.appGreen, //TODO: move text to special class
+        borderSide: const BorderSide(color: AppColors.appGreen));
 
+    var applyButton = _getRoundedButton(() {
+      widget.callback(widget._selectedDate);
+    }, "apply", AppColors.appWhite,
+        backgroundColor: AppColors.appGreen); //TODO: move text to special class
+
+    var column = Column(
+        mainAxisAlignment: MainAxisAlignment.start, 
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _getDatePickerButton(),
+          _getTimePickerButton(),
+          const SizedBox(height: 20),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [cancelButton, applyButton])
+        ]);
+
+    var sizedBox = SizedBox(
+        height: MediaQuery.of(context).size.height * _screenHeightRatio,
+        width: MediaQuery.of(context).size.width * _screenWidthRatio,
+        child: column);
     return AlertDialog(
-      content: SizedBox(
-          height: MediaQuery.of(context).size.height * _screenHeightRatio,
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            _getDatePickerButton(),
-            _getTimePickerButton(),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [cancelButton, applyButton])
-          ])),
+      content: sizedBox,
+      backgroundColor: AppColors.appWhite,
     );
   }
 
@@ -76,7 +86,11 @@ class _DateTimePickerState extends State<DateTimePicker> {
         onPressed: () {
           _handleSelectedDate(context);
         },
-        child: Text(widget._selectedDate.formattedDate));
+        child: Text(widget._selectedDate.formattedDate,
+            style: const TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.w400,
+                color: AppColors.appGreen)));
   }
 
   Widget _getTimePickerButton() {
@@ -84,6 +98,36 @@ class _DateTimePickerState extends State<DateTimePicker> {
         onPressed: () {
           _handleSelectedTime(context);
         },
-        child: Text(widget._selectedDate.formattedTime));
+        child: Text(widget._selectedDate.formattedTime,
+            style: const TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.w300,
+                color: AppColors.appGreen)));
+  }
+
+  ElevatedButton _getRoundedButton(
+      Function() onPressed, String text, Color textColor,
+      {BorderSide borderSide = BorderSide.none,
+      Color backgroundColor = AppColors.appWhite}) {
+    var buttonStyle = ButtonStyle(
+        backgroundColor: MaterialStatePropertyAll(backgroundColor),
+        shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30), side: borderSide)));
+    var textStyle =
+        TextStyle(fontWeight: FontWeight.w300, fontSize: 24, color: textColor);
+
+    double horizontalPadding = 5;
+    double verticalPadding = 12;
+    var button = ElevatedButton(
+        onPressed: onPressed,
+        style: buttonStyle,
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(horizontalPadding, verticalPadding,
+              horizontalPadding, verticalPadding),
+          child: Text(text, //TODO: move text to special class
+              style: textStyle),
+        ));
+    return button;
   }
 }
--- lib/extensions/appColors.dart
+++ lib/extensions/appColors.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static var appBlack = Colors.black;
-  static var appWhite = Colors.white;
+  static const appBlack = Colors.black;
+  static const appWhite = Colors.white;
 
-  static var appGreen = const Color.fromRGBO(0, 133, 150, 1);
-  static var appDisabledGreen = const Color.fromRGBO(120, 186, 195, 1);
-  static var appNumericKeyboardColor = const Color.fromRGBO(238, 238, 238, 1);
+  static const appGreen = Color.fromRGBO(0, 133, 150, 1);
+  static const appDisabledGreen = Color.fromRGBO(120, 186, 195, 1);
+  static const appNumericKeyboardColor = Color.fromRGBO(238, 238, 238, 1);
 }
