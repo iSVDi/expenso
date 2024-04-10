2388697fc031496c1064e60e1b8e580481eab576
Daniil-Shestopalov <d.v.shestopalov@mail.ru>
2024-04-10 16:00:46 +0300

    fix darkTheme

--- lib/extensions/app_colors.dart
+++ lib/extensions/app_colors.dart
@@ -6,6 +6,7 @@ class AppColors {
   static const appWhite = Colors.white;
 
   static const appGreen = Color.fromRGBO(0, 133, 150, 1);
+  static const appDarkGreen = Color.fromRGBO(32, 220, 217, 1);
   static const appDisabledGreen = Color.fromRGBO(120, 186, 195, 1);
   static const appNumericKeyboardColor = Color.fromRGBO(238, 238, 238, 1);
   static const appTimerTextColor = Color.fromRGBO(120, 186, 195, 1);
