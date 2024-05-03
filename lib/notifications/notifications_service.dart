26c7831b7bda02ff997ab3015058c0701ce6687a
Daniil-Shestopalov <d.v.shestopalov@mail.ru>
2024-05-03 18:02:40 +0300

    removed flutter_local_notifications, add todos

--- lib/notifications/notifications_service.dart
+++ lib/notifications/notifications_service.dart
@@ -1,46 +1,9 @@
-import "package:flutter_local_notifications/flutter_local_notifications.dart";
-import 'package:timezone/timezone.dart' as tz;
-import 'package:timezone/data/latest_all.dart' as tz;
 
+//TODO! reminder add handler of displaying notification
 class NotificationsService {
-  final FlutterLocalNotificationsPlugin notificationsPlugin =
-      FlutterLocalNotificationsPlugin();
   static const _reminderNotificationID = 0;
-
   Future<void> initNotification() async {
-    notificationsPlugin
-        .resolvePlatformSpecificImplementation<
-            AndroidFlutterLocalNotificationsPlugin>()
-        ?.requestNotificationsPermission();
-    AndroidInitializationSettings initializationSettingsAndroid =
-        const AndroidInitializationSettings("app_logo");
-
-    var initializationSettingsIOS = DarwinInitializationSettings(
-      requestAlertPermission: true,
-      requestBadgePermission: true,
-      requestSoundPermission: true,
-      onDidReceiveLocalNotification: (id, title, body, payload) {},
-    );
-
-    var initializationSettings = InitializationSettings(
-        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
-
-    await notificationsPlugin.initialize(
-      initializationSettings,
-      onDidReceiveNotificationResponse: (response) {},
-    );
-  }
-
-  _notificationDetails() {
-    return const NotificationDetails(
-      android: AndroidNotificationDetails(
-        "reminder_$_reminderNotificationID",
-        "daily_reminder",
-        importance: Importance.max,
-        priority: Priority.high,
-      ),
-      iOS: DarwinNotificationDetails(),
-    );
+    //TODO! reminder implement
   }
 
   Future scheduleNotifications(
@@ -48,23 +11,10 @@ class NotificationsService {
       required String title,
       required String body,
       required DateTime scheduledNotificationDateTime}) async {
-    tz.initializeTimeZones();
-    return notificationsPlugin.zonedSchedule(
-      id,
-      title,
-      body,
-      tz.TZDateTime.from(
-        scheduledNotificationDateTime,
-        tz.local,
-      ),
-      await _notificationDetails(),
-      uiLocalNotificationDateInterpretation:
-          UILocalNotificationDateInterpretation.absoluteTime,
-      matchDateTimeComponents: DateTimeComponents.time,
-    );
-  }
+        //TODO! reminder implement
+      }
 
   Future cancelNotifications() async {
-    return await notificationsPlugin.cancel(0);
+    //TODO! reminder implement
   }
 }
